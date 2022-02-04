b673e0bc20f7cf29ca677378924e8b979ee93a47
rahim-share <90616198+rahim-share@users.noreply.github.com>
2022-02-04 18:02:04 +0000

    Add files via upload
    Simple script to extract CSV files that our team can use to transform and clean data

---
+++ Extraction of Housing data.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from urllib.request import urlretrieve as retrieve
+
+
+# In[8]:
+
+
+url = 'http://publicdata.landregistry.gov.uk/market-trend-data/house-price-index-data/UK-HPI-full-file-2021-09.csv?utm_medium=GOV.UK&utm_source=datadownload&utm_campaign=full_fil&utm_term=9.30_17_11_21'
+retrieve(url,'HousingData.CSV')
+
+
+# In[7]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
