f90c2132c0f010c9d859c9f8f5b439f7595a2eb1
rahim-share <90616198+rahim-share@users.noreply.github.com>
2022-02-04 19:20:10 +0000

    Using python automation to extract csv files f

---
+++ Extraction Using Selenium.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from selenium import webdriver
+
+
+# In[ ]:
+
+
+options = webdriver.ChromeOptions()
+preferences = {r"download.default_directory": "C:\\Users\\mrahi\\Desktop"} #setting up download directory
+options.add_experimental_option("prefs",preferences)
+
+
+# In[ ]:
+
+
+driver = webdriver.Chrome(r"C:\\Users\\mrahi\\Desktop\\chromedriver.exe")
+driver.get("https://www.gov.uk/government/statistical-data-sets/uk-house-price-index-data-downloads-september-2021#download-the-data")
+driver.path_element(By.XPATH,"/html[@class='govuk-template show-global-bar']/body[@class='gem-c-layout-for-public govuk-template__body js-enabled']/div[@id='wrapper']/main[@id='content']/div[@class='govuk-grid-row'][3]/div[@class='govuk-grid-column-two-thirds']/div[@id='contents']/div[@class='responsive-bottom-margin'][2]/div[@class='gem-c-govspeak govuk-govspeak direction-ltr']/div[@class='govspeak']/ul[1]/li/a[@class='govuk-link']").click()
+
+
+# In[ ]:
+
+
+
+
